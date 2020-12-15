586b202a29d82292d70fad7582fa75457528882f
bkellerman <bert@ciphertrace.com>
2020-12-15 12:01:41 -0800

    remove old docs; update testchain name

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 #CONFIG="testchain-value-english-governance-median-multisig-basic"
 #CONFIG="testchain-value-fixed-discount-governance-median-multisig-basic"
-CONFIG="testchain-value-fixed-discount-uniswap-multisig-basic"
+CONFIG="rai-testchain-value-fixed-discount-uniswap-multisig-basic"
 while getopts :c:f: option
 do
 case "${option}"
