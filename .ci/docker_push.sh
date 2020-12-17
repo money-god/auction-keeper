953ec4d2523e0cbb850fecef1f22d9af8297231c
bkellerman <bert@ciphertrace.com>
2020-12-17 12:50:24 -0800

    fix deploy process

--- .ci/docker_push.sh
+++ .ci/docker_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t reflexer/auction-keeper .
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker tag reflexer/auction-keeper reflexer/auction-keeper:$COMMIT
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+docker tag reflexer/auction-keeper reflexer/auction-keeper:${COMMIT}
 docker push reflexer/auction-keeper
-docker push reflexer/auction-keeper:$COMMIT
+docker push reflexer/auction-keeper:${COMMIT}
